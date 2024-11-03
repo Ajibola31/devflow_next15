910297655cd6b646719789b0c0890faa7314dde8
Ajibola31 <ajiquinine@gmail.com>
2024-11-03 23:33:39 +0100

    ESLint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
-
 const Home = () => (
-    <h1 className="text-3xl text-violet-700 ">Welcome to Devflow</h1>
-  );
+  <h1 className="text-3xl font-black text-white ">Welcome to Devflow</h1>
+);
 
 export default Home;
- 
