c8328133c53c1b631b3664804d202b2f694a27dc
Ajibola31 <ajiquinine@gmail.com>
2024-11-08 10:05:17 +0100

    Access & Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-const Home = () => <h1 className="h1-bold">Welcome to Devflow</h1>;
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome to Devflow</h1>
+    <h1 className="h1-bold font-space-grotesk">Welcome to Devflow</h1>
+  </>
+);
 
 export default Home;
